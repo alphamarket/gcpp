765f1f51257607b2633d465673d728f5dc881f88
noise2 <b.g.dariush@gmail.com>
2015-03-30 02:25:29 +0430

    @ gcpp.1/gc_ptr.hpp: structe design improved

--- gcpp.1/gc_ptr.hpp
+++ gcpp.1/gc_ptr.hpp
@@ -4,41 +4,44 @@
 #include <iostream>
 #include <assert.h>
 #include <algorithm>
+#include <stdexcept>
 #include <unordered_map>
 #include "gcafx.hpp"
 namespace gc {
 #   define ptoi(p) reinterpret_cast<std::intptr_t>(p)
 #   define where typename = typename
+#   define deleted_signature(_T) void(*)(_T*)
     using namespace std;
-    typedef struct gc_weak_ptr_str {
-        std::weak_ptr<void> wp;
-        bool is_ref_casted;
-        template<typename T>
-        gc_weak_ptr_str(const std::shared_ptr<T>& gp, bool is_ref_casted = false) {
-            wp = gp;
-            this->is_ref_casted = is_ref_casted;
-        }
-    } gc_weak_ptr_str;
-
-    typedef std::unordered_multimap<void*, gc_weak_ptr_str> gc_map;
-    static gc_map map;
-    template<typename T, typename = typename std::enable_if<!std::is_pointer<T>::value>::type>
-    class gc_ptr : public std::shared_ptr<void> {
+    /**
+     * class gc_ptr decl. this class is to manage pointers to objects
+     * so it is not logical to accept pointers as input
+     */
+    template<typename T, where
+        std::enable_if<
+            !std::is_pointer<T>::value>::type>
+    class gc_ptr                                                    : public std::shared_ptr<void> {
         typedef std::shared_ptr<void>   base;
-#       define deleted_signature(_T) void(*)(_T*)
         typedef gc_ptr                  self;
-        template<typename _T>
-            friend gc_ptr<_T> ref_cast(_T*);
-        template<typename _T>
-            friend class gc_node;
-        template<typename _T>
-            friend class gc_weak_ptr;
-        friend struct gc_weak_ptr_str;
     protected:
-        static void dont_delete(T*) { cout<<"[DELX]"; }
-        void*   _data = NULL;
         /**
-         * for general pointer delete constructor
+         * the containing data
+         */
+        void*           _data = nullptr;
+        /**
+         * The possible events enum
+         */
+        typedef enum    EVENT { E_CTOR, E_DTOR, E_DELETE } EVENT;
+        /**
+         * The do not delete flag for stop deletion
+         * at the end of contained pointer's life
+         */
+        template<typename _T = T, where
+            std::enable_if<
+                std::is_convertible<T, _T>::value>::type>
+        static void dont_delete(T* p)
+        { _event(E_DELETE, p); }
+        /**
+         * for general pointer delete constructor [ The return sorce of other ctor ]
          */
         template<typename _T, typename _Delete, where
             std::enable_if<
@@ -46,6 +49,26 @@ namespace gc {
         gc_ptr(_T* p, _Delete d)
             : base(p, d)
         { assert(*std::get_deleter<deleted_signature(_T)>(*this) == d); }
+        /**
+         * @brief event operator
+         * @param e the event
+         * @param p
+         */
+        static void _event(EVENT e, __unused const void* const p = nullptr) {
+            switch(e) {
+                case E_CTOR:
+                    cout<<"[CTOR: "<<((self*)p)->get()<<"]"<<endl;
+                    break;
+                case E_DTOR:
+                    cout<<"[DTOR: "<<((self*)p)->get()<<"]"<<endl;
+                    break;
+                case E_DELETE:
+                    cout<<"[DELX: "<<p<<"]"<<endl;
+                    break;
+                default:
+                    throw std::invalid_argument("Invalid event passed!");
+            }
+         }
     public:
         /**
          * get the internal pointer with a cast
@@ -59,13 +82,13 @@ namespace gc {
          * the dtor
          */
         ~gc_ptr()
-        { }
+        { _event(E_DTOR, this); }
         /**
          * for empty ptr
          */
         gc_ptr()
-            : base(nullptr, self::dont_delete)
-        { }
+            : self(static_cast<T*>(nullptr), self::dont_delete)
+        { _event(E_CTOR, this); }
         /**
          * for copy assignments [ only for convertable data types ]
          */
@@ -73,8 +96,8 @@ namespace gc {
             std::enable_if<
                 std::is_convertible<_T, T>::value>::type>
         gc_ptr(const gc_ptr<_T>& gp)
-            : self(gp.get())
-        { }
+            : self(gp.get(), self::dont_delete)
+        { _event(E_CTOR, this); }
         /**
          * for general pointer assignment [only on no<-conversion types between the class' T and the input _T]
          */
@@ -84,7 +107,7 @@ namespace gc {
                 std::is_same<T, _T>::value>::type>
         gc_ptr(_T* p)
             : self(p, self::dont_delete)
-        { }
+        { _event(E_CTOR, this); }
         /**
          * for stack var assignments
          */
@@ -95,8 +118,8 @@ namespace gc {
                 !std::is_pointer<_Tin>::value>::type>   // this cond. made and ~this cond. make in the
                                                         // below assertion to make sure we stop the stack setting:)
         gc_ptr(const _Tin& p)
-            : self(const_cast<_Tin*>(std::addressof(p)))
-        { static_assert(std::is_pointer<_Tin>::value, "Cannot assign stack varibales as managed pointers!"); }
+            : self(const_cast<_Tin*>(std::addressof(p)), self::dont_delete)
+        { _event(E_CTOR, this); static_assert(std::is_pointer<_Tin>::value, "Cannot assign stack varibales as managed pointers!"); }
         /**
          * for base <- derived assignments [only for derived types, does not accept the same type]
          */
@@ -106,7 +129,7 @@ namespace gc {
                 std::is_base_of<B, D>::value>::type>
         gc_ptr(D* p)
             : self(dynamic_cast<B*>(p), self::dont_delete)
-        { }
+        { _event(E_CTOR, this); }
         /**
          * assignment oprator
          */
@@ -115,12 +138,13 @@ namespace gc {
                 std::is_convertible<_T, T>::value>::type>
         inline self& operator =(const gc_ptr<_T>& gp) {
             if(auto del_p = std::get_deleter<deleted_signature(T)>(gp))
-                this->reset(gp.get<T>(), *del_p);
+                *this = self(gp.get<T>(), *del_p);
             else
-                this->reset(gp.get<T>());
+                *this = self(gp.get<T>());
             return *this;
         }
     };
+#   undef deleted_signature
 #   undef where
 #   undef ptoi
 }
