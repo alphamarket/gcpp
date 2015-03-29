bba6fa58afb3b0121b3dde501b60934423d9300e
noise2 <b.g.dariush@gmail.com>
2015-03-29 05:19:17 +0430

    @ gc_ptr.hpp: +::ref_cast<> is done

--- gcpp.1/gc_ptr.hpp
+++ gcpp.1/gc_ptr.hpp
@@ -8,21 +8,11 @@ namespace gc {
     template<typename T>
     class gc_ptr : public base_gc_ptr(T) {
     public:
-        gc_ptr<T>& as_ref() {
-//            *this = base_gc_ptr(T)(this, [](T*) { });
-            return *this;
-        }
-
 #       include "inc/gc_ptr.ctor.inc"
 #       include "inc/gc_ptr.opr.inc"
     };
-    template<typename T>
-    gc_ptr<T>& gc_ref(T* p) {
-    #define __prevent_del [](T*)->void{cout<<"DELETE PREVENTED"<<endl;}
-        return gc_ptr<T>(p, __prevent_del);
-    #undef __prevent_del
-    }
-
+    template <typename T>
+    gc_ptr<T> ref_cast(T* p) { return gc_ptr<T>(p, [](T*)->void{}); }
 #   undef base_gc_ptr
 }
 
