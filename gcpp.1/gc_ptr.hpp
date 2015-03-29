d4eb818f833adc95f4de1ef95dcc0d7944c0b5e4
noise2 <b.g.dariush@gmail.com>
2015-03-29 06:03:56 +0430

    @ gc_ptr.hpp: good fixes

--- gcpp.1/gc_ptr.hpp
+++ gcpp.1/gc_ptr.hpp
@@ -1,12 +1,16 @@
 #ifndef HDR__GC_PTR_HPP
 #define HDR__GC_PTR_HPP
-
 #include <memory>
+#include "gcafx.hpp"
 
 namespace gc {
     template<typename T>
     class gc_ptr : public std::shared_ptr<void> {
-        typedef std::shared_ptr<void> base;
+        typedef std::shared_ptr<void>   base;
+        typedef gc_ptr                  self;
+        template <typename _T> gc_ptr<_T> ref_cast(_T*);
+    protected:
+        static void __prevent_delete(__unused T*) { }
     public:
         void reset() { cout<<"RESET"; base::reset(); }
 
@@ -25,7 +29,7 @@ namespace gc {
 #       include "inc/gc_ptr.opr.inc"
     };
     template <typename T>
-    gc_ptr<T> ref_cast(T* p) { return gc_ptr<T>(p, [](T*)->void{}); }
+    gc_ptr<T> ref_cast(T* p) { return gc_ptr<T>(p, gc::gc_ptr<T>::__prevent_delete); }
 #   undef base_gc_ptr
 }
 
