86af7af88a5661b0bdf31b121a88c9bbbb24e225
noise2 <b.g.dariush@gmail.com>
2015-03-30 03:50:58 +0430

    @ gcpp.1/gc_ptr.hpp: HUGE imropvements, working on dynamic cast op. problem

--- gcpp.1/gc_ptr.hpp
+++ gcpp.1/gc_ptr.hpp
@@ -8,9 +8,10 @@
 #include <unordered_map>
 #include "gcafx.hpp"
 namespace gc {
-#   define ptoi(p) reinterpret_cast<std::intptr_t>(p)
+#   define ptoi(p) reinterpret_cast<std::intptr_Tin>(p)
 #   define where typename = typename
-#   define deleted_signature(_T) void(*)(_T*)
+#   define deleted_signature(_Tin) void(*)(_Tin*)
+#   define should_dynamic_cast(BASE, DRIVED) !std::is_same<BASE, DRIVED>::value && std::is_class<BASE>::value && std::is_base_of<BASE, DRIVED>::value
     using namespace std;
     /**
      * class gc_ptr decl. this class is to manage pointers to objects
@@ -23,6 +24,7 @@ namespace gc {
         typedef std::shared_ptr<void>   base;
         typedef gc_ptr                  self;
     protected:
+        bool            is_stack = false;
         /**
          * the containing data
          */
@@ -30,25 +32,26 @@ namespace gc {
         /**
          * The possible events enum
          */
-        typedef enum    EVENT { E_CTOR, E_DTOR, E_DELETE } EVENT;
+        enum    class EVENT { E_CTOR, E_DTOR, E_MOVE, E_DELETE };
         /**
          * The do not delete flag for stop deletion
          * at the end of contained pointer's life
          */
-        template<typename _T = T, where
+        template<typename _Tin = T, where
             std::enable_if<
-                std::is_convertible<T, _T>::value>::type>
+                std::is_convertible<T, _Tin>::value>::type>
         static void dont_delete(T* p)
-        { _event(E_DELETE, p); }
+        { _event(EVENT::E_DELETE, p); }
         /**
          * for general pointer delete constructor [ The return sorce of other ctor ]
          */
-        template<typename _T, typename _Delete, where
+        template<typename _Tin, typename _Delete, where
             std::enable_if<
-                std::is_convertible<_T, T>::value>::type>
-        gc_ptr(_T* p, _Delete d)
+                std::is_pointer<_Delete>::value &&
+                std::is_convertible<_Tin, T>::value>::type>
+        inline gc_ptr(_Tin* p, _Delete d)
             : base(p, d)
-        { assert(*std::get_deleter<deleted_signature(_T)>(*this) == d); }
+        { assert(*std::get_deleter<deleted_signature(_Tin)>(*this) == d); }
         /**
          * @brief event operator
          * @param e the event
@@ -56,58 +59,75 @@ namespace gc {
          */
         static void _event(EVENT e, __unused const void* const p = nullptr) {
             switch(e) {
-                case E_CTOR:
-                    cout<<"[CTOR: "<<((self*)p)->get()<<"]"<<endl;
+                case EVENT::E_CTOR:
+                    cout<<"[CTOR: "<<((self*)p)->get()<<"]";
                     break;
-                case E_DTOR:
-                    cout<<"[DTOR: "<<((self*)p)->get()<<"]"<<endl;
+                case EVENT::E_DTOR:
+                    cout<<"[DTOR: "<<((self*)p)->get()<<"]";
                     break;
-                case E_DELETE:
+                case EVENT::E_MOVE:
+                    cout<<"[MOVE: "<<((self*)p)->get()<<"]";
+                    break;
+                case EVENT::E_DELETE:
                     cout<<"[DELX: "<<p<<"]"<<endl;
-                    break;
+                    return;
                 default:
                     throw std::invalid_argument("Invalid event passed!");
             }
+            if(((self*)p)->is_stack)
+                cout<<"(ON STACK)";
+            cout<<endl;
          }
     public:
         /**
          * get the internal pointer with a cast
          */
-        template<typename _T = T, where
+        template<typename _Tout = T, where
             std::enable_if<
-                std::is_convertible<T, _T>::value>::type>
-        _T* get() const
-        { return static_cast<_T*>(base::get()); }
+                std::is_convertible<T, _Tout>::value>::type>
+        inline _Tout* get() const
+        {
+            if(should_dynamic_cast(_Tout, T))
+                return dynamic_cast<_Tout*>(base::get());
+            return static_cast<_Tout*>(base::get());
+        }
         /**
          * the dtor
          */
-        ~gc_ptr()
-        { _event(E_DTOR, this); }
+        inline ~gc_ptr()
+        { _event(EVENT::E_DTOR, this); }
         /**
          * for empty ptr
          */
-        gc_ptr()
+        inline gc_ptr()
             : self(static_cast<T*>(nullptr), self::dont_delete)
-        { _event(E_CTOR, this); }
+        { _event(EVENT::E_CTOR, this); }
         /**
          * for copy assignments [ only for convertable data types ]
          */
-        template<typename _T, where
+        template<typename _Tin, where
             std::enable_if<
-                std::is_convertible<_T, T>::value>::type>
-        gc_ptr(const gc_ptr<_T>& gp)
-            : self(gp.get(), self::dont_delete)
-        { _event(E_CTOR, this); }
+                std::is_convertible<_Tin, T>::value>::type>
+        inline gc_ptr(const gc_ptr<_Tin>& gp)
+            : self(gp.get(), *std::get_deleter<deleted_signature(T)>(gp))
+        { _event(EVENT::E_CTOR, this); }
         /**
-         * for general pointer assignment [only on no<-conversion types between the class' T and the input _T]
+         * for move assignments [ only for convertable data types ]
          */
-        template<typename _T, where
+        template<typename _Tin, where
             std::enable_if<
-                !std::is_reference<_T>::value ||
-                std::is_same<T, _T>::value>::type>
-        gc_ptr(_T* p)
+                std::is_convertible<_Tin, T>::value>::type>
+        inline gc_ptr(const gc_ptr<_Tin>&& gp)
+        {  *this = std::move(gp); _event(EVENT::E_MOVE, this); }
+        /**
+         * for general pointer assignment [only on no<-conversion types between the class' T and the input _Tin]
+         */
+        template<typename _Tin, where
+            std::enable_if<
+                std::is_same<T, _Tin>::value>::type>
+        inline gc_ptr(_Tin* p, bool stack_alloced = false)
             : self(p, self::dont_delete)
-        { _event(E_CTOR, this); }
+        { this->is_stack = stack_alloced; _event(EVENT::E_CTOR, this); }
         /**
          * for stack var assignments
          */
@@ -117,26 +137,27 @@ namespace gc {
                 std::is_convertible<_Tin, T>::value &&
                 !std::is_pointer<_Tin>::value>::type>   // this cond. made and ~this cond. make in the
                                                         // below assertion to make sure we stop the stack setting:)
-        gc_ptr(const _Tin& p)
+        inline gc_ptr(const _Tin& p)
             : self(const_cast<_Tin*>(std::addressof(p)), self::dont_delete)
-        { _event(E_CTOR, this); static_assert(std::is_pointer<_Tin>::value, "Cannot assign stack varibales as managed pointers!"); }
+        { _event(EVENT::E_CTOR, this); static_assert(std::is_pointer<_Tin>::value, "Cannot assign stack varibales as managed pointers!"); }
         /**
          * for base <- derived assignments [only for derived types, does not accept the same type]
          */
         template<typename D, typename B = T, where
             std::enable_if<
+                std::is_class<B>::value &&
                 !std::is_same<B, D>::value &&
                 std::is_base_of<B, D>::value>::type>
-        gc_ptr(D* p)
+        inline gc_ptr(D* p, bool stack_alloced = false)
             : self(dynamic_cast<B*>(p), self::dont_delete)
-        { _event(E_CTOR, this); }
+        { this->is_stack = stack_alloced; _event(EVENT::E_CTOR, this); }
         /**
          * assignment oprator
          */
-        template<typename _T, where
+        template<typename _Tin, where
             std::enable_if<
-                std::is_convertible<_T, T>::value>::type>
-        inline self& operator =(const gc_ptr<_T>& gp) {
+                std::is_convertible<_Tin, T>::value>::type>
+        inline self& operator =(const gc_ptr<_Tin>& gp) {
             if(auto del_p = std::get_deleter<deleted_signature(T)>(gp))
                 *this = self(gp.get<T>(), *del_p);
             else
@@ -144,6 +165,19 @@ namespace gc {
             return *this;
         }
     };
+    /**
+     * converts a reference pointer to a stack varibale to gc_ptr
+     */
+    template<typename _Tout, typename _Tin, where
+        std::enable_if<
+            std::is_convertible<_Tin, _Tout>::value>::type>
+    inline gc::gc_ptr<_Tout> ref2ptr(_Tin* ref)
+    {
+        if(should_dynamic_cast(_Tout, _Tin))
+            return gc::gc_ptr<_Tout>(dynamic_cast<_Tout*>(ref), true);
+        return gc::gc_ptr<_Tout>(static_cast<_Tout*>(ref), true);
+    }
+#   undef should_dynamic_cast
 #   undef deleted_signature
 #   undef where
 #   undef ptoi
